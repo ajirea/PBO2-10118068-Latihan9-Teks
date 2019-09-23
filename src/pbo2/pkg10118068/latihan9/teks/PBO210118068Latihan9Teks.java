915489e362abd87b0553dfcbde030f9d8d328440
Satria Aji Putra <satriamaxt@gmail.com>
2019-09-23 19:13:00 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan9/teks/PBO210118068Latihan9Teks.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan9.teks;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Belajar mendeklarasikan variable teks beserta mengenal
+ * fungsi fungsi pada teks seperti newline dan tab beserta concat
+ */
+public class PBO210118068Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        
+        System.out.println("Hasil: " + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
